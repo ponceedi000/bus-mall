19289a94e7debfaebabc030e18c859d9631feca9
Ediberto Ponce <ediberto01@gmail.com>
2021-08-21 16:35:25 -0700

    Added the use of local storage to my .js file

--- js/app.js
+++ js/app.js
@@ -1,19 +1,15 @@
 'use strict';
-
-// global varriables
-
+// Beginning of global variables
 let allProducts = [];
 let clicks = 0;
 let clickAllowed = 25;
 let numberOfUniqueIndexes = 6;
-
 let myContainer = document.querySelector('section');
 let image1 = document.querySelector('section img:first-child');
 let image2 = document.querySelector('section img:nth-child(2)');
 let image3 = document.querySelector('section img:nth-child(3)');
 let indexArray = [];
-
-
+// End of global variables
 function Product(name, fileExtension = 'jpg') {
   this.name = name;
   this.src = `img/${name}.${fileExtension}`;
@@ -21,7 +17,7 @@ function Product(name, fileExtension = 'jpg') {
   this.views = 0;
   allProducts.push(this);
 }
-
+// New instances
 new Product('bag');
 new Product('banana');
 new Product('bathroom');
@@ -42,10 +38,11 @@ new Product('unicorn')
 new Product('water-can')
 new Product('wine-glass')
 
+// Random product function
 function selectRandomProduct() {
   return Math.floor(Math.random() * allProducts.length);
 }
-
+// This functions ensures that there are no duplicate images for at least (2) back-to-back iterations
 function renderProducts() {
   while (indexArray.length < numberOfUniqueIndexes) {
     let randomNumber = selectRandomProduct();
@@ -58,19 +55,31 @@ function renderProducts() {
   let product2 = indexArray.shift();
   let product3 = indexArray.shift();
 
-image1.src = allProducts[product1].src;
-image1.alt = allProducts[product1].name;
-allProducts[product1].views++;
-image2.src = allProducts[product2].src;
-image2.alt = allProducts[product2].name;
-allProducts[product2].views++;
-image3.src = allProducts[product3].src;
-image3.alt = allProducts[product3].name;
-allProducts[product3].views++;
-console.log(indexArray);
-}
-//---End of renderProducts function---//
+  image1.src = allProducts[product1].src;
+  image1.alt = allProducts[product1].name;
+  allProducts[product1].views++;
+  image2.src = allProducts[product2].src;
+  image2.alt = allProducts[product2].name;
+  allProducts[product2].views++;
+  image3.src = allProducts[product3].src;
+  image3.alt = allProducts[product3].name;
+  allProducts[product3].views++;
+  console.log(indexArray);
+} //---End of renderProducts function---//
 
+// creates storage
+function storeAProduct() {
+  let stringifyProducts = JSON.stringify(allProducts);
+  localStorage.setItem('productstorage',stringifyProducts);
+}
+// retrieves from local storage
+function getProducts() {
+  let potentialProducts = localStorage.getItem('productstorage');
+  if(potentialProducts) {
+    let parsedProducts = JSON.parse(potentialProducts);
+    allProducts = parsedProducts;
+  }
+}
 function handleProductClick(event) {
   if (event.target === myContainer) {
     alert('Please click on an image');
@@ -87,11 +96,11 @@ function handleProductClick(event) {
   if (clicks === clickAllowed) {
     myContainer.removeEventListener('click', handleProductClick);
     renderChart();
+    storeAProduct(); //calling created storage
   }
 }
 renderProducts();
-
-
+// Chart.js
 function renderChart() {
   let productViews = [];
   let productClicks = [];
@@ -102,7 +111,6 @@ function renderChart() {
     productNames.push(allProducts[i].name);
     
   }
-  //console.log(productClicks);
   let chartObject = {
     type: 'bar',
     data: {
@@ -136,3 +144,4 @@ function renderChart() {
 }
 
 myContainer.addEventListener('click', handleProductClick);
+getProducts(); //function created on line 76 for retrieving local storage
